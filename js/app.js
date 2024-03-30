4d7408c6e6d390312c96fedc198d0855d55c255b
telmo-checa <telmocheca1@gmail.com>
2024-03-30 22:36:02 +0100

    correzione id html, recupero elementi del DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("ciao ")
+console.log("ciao ");
+
+// recupero elementi del DOM
+
+const nomeElementForm = document.getElementById("nome")
+console.dir(nomeElementForm);
+const cognomeElementForm = document.getElementById("cognome")
+const emailElementForm = document.getElementById("email")
+const prestazioniElementForm = document.getElementById("prestazione")
+const descrizioneElementForm = document.getElementById("descrizione")
+const codePromoElementForm = document.getElementById("code-promo")
+const prezzoElementForm = document.getElementById("prezzo")
