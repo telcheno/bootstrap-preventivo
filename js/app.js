ed65f643c4b5678882e6ae95a525f2fc1654dc90
telmo-checa <telmocheca1@gmail.com>
2024-04-04 20:04:31 +0200

    ADD: ciclo for e pulizia dei console.log

--- js/app.js
+++ js/app.js
@@ -1,37 +1,31 @@
 console.log("ciao ");
 
-// recupero elementi del DOM con il tipo di "VALORE" e lo "SCOPE" delle variabili
+                //RECUPERO ELEMENTI DEL DOM CON IL TIPO DI "VALORE"
+
 // asegno il valore degli elementi perrichiamarli
-const nomeElementForm = document.getElementById("nome")//stringa
-// console.dir(nomeElementForm);
+const nomeElementForm = document.getElementById("nome");//stringa
 
-const cognomeElementForm = document.getElementById("cognome")
-// console.dir(cognomeElementForm);
+const cognomeElementForm = document.getElementById("cognome");//stringa
 
+const emailElementForm = document.getElementById("email");//stringa
 
-const emailElementForm = document.getElementById("email")
-// console.dir(emailElementForm);
-
-// elementi per il calcolo del preventivo
+                //ELEMENTI PER IL CALCOLO DEL PREVENTIVO
 
 // tipo di lavoro da clacolare
-const prestazioniElementForm = document.getElementById("prestazione")
-// console.dir(prestazioniElementForm);
-
+const prestazioniElementForm = document.getElementById("prestazione");//stringa
 
 //descrizione del lavoro
-const descrizioneElementForm = document.getElementById("descrizione")
+const descrizioneElementForm = document.getElementById("descrizione");//stringa
 
 //codici per lo sconro da calcorare
-const codePromoElementForm = document.getElementById("code-promo")
-// console.dir(codePromoElementForm);
+const codePromoElementForm = document.getElementById("code-promo");//stringa
 
 //prezzo da stampare nel html
-const prezzoElementForm = document.getElementById("prezzo")
-// console.dir(prezzoElementForm);
+const prezzoElementForm = document.getElementById("prezzo");//stringa
 
-//section form  
-const preventivoElementForm = document.getElementById("from-preventivo");
+                        //SECTION FORM
+
+const preventivoElementForm = document.getElementById("from-preventivo");//stringa
 // console.dir(preventivoElementForm);
 
 //agiungo eveneto "submti" al elemento form
@@ -43,45 +37,61 @@ preventivoElementForm.addEventListener("submit", function(event){
   //recupero i "value" de gli elementi 
   const scelta = prestazioniElementForm.value;//string
   const codidePromo = codePromoElementForm.value;//string
-  let costoOraLavoro = 0 //number
-  let tempoLavoro = 10 //number
-  let prezzoBase = 0 //number
+
+  //variabili info
+  let costoOraLavoro = 0; //number
+  let tempoLavoro = 10; //number
+  let prezzoBase = 0; //number
     
-  //condizionale per il consto per ogni prestazione
+          //CONDIZIONALE PER LA SCELTA DELLA PRESTAZIONE E ASSEGNAZIONE COSTO ORARIO
 
   //SE "scelta" e === "backend"
   if (scelta === "backend"){
   //--"costoOraLavoro" = 20.50
     costoOraLavoro = 20.50
-    console.log(costoOraLavoro);
+
   //ALTRIMENTI SE "scelta" === "fronted"" 
   }else if (scelta === "fronted"){
       //--"costoOraLavoro" = 15.30
       costoOraLavoro = 15.30
-      console.log(costoOraLavoro);
+      
   //ALTRIMENTI SE "scelta" === "analisi"
   }else if (scelta === "analisi") {
       //--"costoOraLavoro" = 33.60
     costoOraLavoro = 33.60
-    console.log(costoOraLavoro);  
+
   // ALTRIMENTI
   }else{
     //--invio "alert" per informare che si deve scegliere un opzione
-    alert("Per il calcolo del preventivo devi scegliere una opzione di lavoro")
+    alert("Per il calcolo del preventivo devi scegliere una opzione di lavoro");
   }
 
-  //controllo del inserimento del codice promozionale
+            //CONTROLLO DEL CODICE PROMOZIONALE
+
   //creo un "array" con i codici promo da controlare
-  //creo una variabile con  "codice cliente" con il varore "false"  pre controlo iterazione
+  const codiciPromo = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+  //creo una variabile con  "codice cliente"
+  const codSconto = codePromoElementForm.value;
+
   //creo variabile "sconto" = 0
+  let percentualeSconto = 0;//NUMBER
+
   //facio un ciclo "for" per confrontare il "codice cliente" con il "codigo promo"
-  //"indice corrent" = indice del "array"
-  //--con una condizionale
-  //--SE "codice cliente" === "indice corrente" 
-  //--"codice cliente" = true
-  //--"sconto" = 25%
-  //ALTRIMENTI
-  //--"sconto" = 0
+  for(let i = 0; i < codiciPromo.length; i++){
+    //"indice corrent" = indice del "array"
+    const codiceCorrente = codiciPromo[i];
+
+          //USO UNA CONDIZIONALE
+    //--SE "codice cliente" === "indice corrente" 
+    if (codiceCorrente === codSconto){
+      //--"sconto" = 25%
+      percentualeSconto = 2.5;
+    }
+
+           //CREO UNA FUNCTIONPER IL CALCOLO DEL PERVENTIVO
+    
+  }
 })
 
 
